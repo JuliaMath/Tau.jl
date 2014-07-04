1028680291cad8fdb7c34d0472831c2a8207ab7a
Anthony Erlinger <aerlinger@gmail.com>
2014-07-04 16:23:23 -0700

    Add BigFloat conversion compatibility

--- src/Tau.jl
+++ src/Tau.jl
@@ -5,8 +5,11 @@ export tau, τ
 macro math_const(sym, val, def)
     esym = esc(sym)
     qsym = esc(Expr(:quote, sym))
+
     bigconvert = quote
-        Base.convert(::Type{BigFloat}, ::MathConst{$qsym}) = $(esc(def))
+      # Long version of Tau isn't defined in MPFR library so it's defined manually here:
+        Base.convert(::Type{BigFloat}, ::MathConst{:τ}) = BigFloat(6.283185307179586476925286766559005768394338798750211641949889184615632812572417997256069650684234135964296173026564613294187689219101164463450718816256)
+        big(x::MathConst{:τ}) = Base.convert(BigFloat, τ)
     end
 
     quote
@@ -20,4 +23,6 @@ end
 # Use overridden macro definition to define conversion methods for tau
 @math_const τ        6.28318530717958647692  tau
 const tau = τ
+
+
 end
