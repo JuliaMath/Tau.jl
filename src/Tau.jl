341bf215a8957b551e933b3b93097793697e2ff8
Mosè Giordano <giordano@users.noreply.github.com>
2017-05-09 12:08:07 +0200

    Enable precompilation (#18)

--- src/Tau.jl
+++ src/Tau.jl
@@ -1,3 +1,7 @@
+# Precompile the module:
+# https://docs.julialang.org/en/latest/manual/modules.html#Module-initialization-and-precompilation-1
+__precompile__()
+
 module Tau
 export tau, τ,
        sintau, costau, modtau,
