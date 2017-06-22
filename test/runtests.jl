a5f6cabeda6a11630bda8141dc18a977f8e5e796
Waldir Pimenta <waldyrious@gmail.com>
2017-06-22 13:49:45 +0100

    use the @testset macro to group the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,65 +1,91 @@
 using Tau
 using Base.Test
 
-# Basic tests
-@test tau ≠ 2 * pi # tau is Irrational, can't be equal to an AbstractFloat
-@test Float32(tau) == 2*Float32(pi)
-@test Float64(Float32(tau)) == Float64(2*Float32(pi))
-@test big(tau) == 2(big(pi))
-@test isa(tau, Irrational)
-
-# Test approximate results of sintau/costau with real inputs
-for T = (Float32, Float64), x = -3:0.01:3.0
-    @test @inferred(sintau(T(x))) ≈ T(sinpi(2 * parse(BigFloat, string(x))))
-    @test @inferred(costau(T(x))) ≈ T(cospi(2 * parse(BigFloat, string(x))))
+@testset "basic tests" begin
+    @test tau ≠ 2 * pi # tau is Irrational, can't be equal to an AbstractFloat
+    @test Float32(tau) == 2*Float32(pi)
+    @test Float64(Float32(tau)) == Float64(2*Float32(pi))
+    @test big(tau) == 2(big(pi))
+    @test isa(tau, Irrational)
 end
 
-# Test approximate results of sintau/costau with complex inputs
-for x in -3.0:0.1:3.0, y in -3.0:0.1:3.0
-    z = complex(x, y)
-    @test @inferred(sintau(z)) ≈ sinpi(2 * z)
-    @test @inferred(costau(z)) ≈ cospi(2 * z)
+@testset "sintau/costau" begin
+
+    @testset "approximate results for fractional inputs" begin
+
+        @testset "real values" begin
+            for T = (Float32, Float64), x = -3:0.01:3.0
+                @test @inferred(sintau(T(x))) ≈ T(sinpi(2 * parse(BigFloat, string(x))))
+                @test @inferred(costau(T(x))) ≈ T(cospi(2 * parse(BigFloat, string(x))))
+            end
+        end
+
+        @testset "complex values" begin
+            for x in -3.0:0.1:3.0, y in -3.0:0.1:3.0
+                z = complex(x, y)
+                @test @inferred(sintau(z)) ≈ sinpi(2 * z)
+                @test @inferred(costau(z)) ≈ cospi(2 * z)
+            end
+        end
+
+    end
+
+    @testset "exact results for integer inputs" begin
+
+        @testset "real and complex values passed as integer types" begin
+            for T = (Int, Complex), x = -3:3
+                @test @inferred(sintau(T(x))) == zero(T)
+                @test @inferred(costau(T(x))) == one(T)
+            end
+        end
+
+        @testset "real and complex values passed as floating point types" begin
+            for T = (Float32, Float64, BigFloat, Complex), x = -3.0:3.0
+                @test @inferred(sintau(T(x))) == sign(x)*zero(T)
+                @test @inferred(costau(T(x))) == one(T)
+            end
+        end
+
+    end
+
+    @testset "corner cases for abnormal inputs" begin
+
+        @testset "real values" begin
+            for x in (Inf, -Inf)
+                @test_throws DomainError sintau(x)
+                @test_throws DomainError costau(x)
+            end
+            @test isequal(@inferred(sintau(NaN)), sinpi(NaN))
+            @test isequal(@inferred(costau(NaN)), cospi(NaN))
+        end
+
+        @testset "complex values" begin
+            for x in (Inf, NaN, 0), y in (Inf, NaN, 0)
+                z = complex(x, y)
+                @test isequal(@inferred(sintau(z)), sinpi(2 * z))
+                @test isequal(@inferred(costau(z)), cospi(2 * z))
+            end
+        end
+
+    end
+
+    # Adapted from julia/test/math.jl
+    @testset "type stability" begin
+        for T = (Int,Float32,Float64,BigFloat), f = (sintau,costau)
+            @test Base.return_types(f,Tuple{T}) == [T]
+            @test Base.return_types(f,Tuple{Complex{T}}) == [Complex{float(T)}]
+        end
+    end
 end
 
-# Test exact results of sintau/costau with real and complex integer inputs
-for T = (Int, Complex), x = -3:3
-    @test @inferred(sintau(T(x))) == zero(T)
-    @test @inferred(costau(T(x))) == one(T)
+# Adapted from julia/test/mod2pi.jl
+@testset "modtau" begin
+    @test_throws ArgumentError modtau(Int64(2)^60-1)
+    @test modtau(10) ≈ mod(10, tau)
+    @test modtau(-10) ≈ mod(-10, tau)
+    @test modtau(355) ≈ 3.1416227979431572
+    @test modtau(Int32(355)) ≈ 3.1416227979431572
+    @test modtau(355.0) ≈ 3.1416227979431572
+    @test modtau(355.0f0) ≈ 3.1416228f0
+    @test modtau(Int64(2)^60) == modtau(2.0^60)
 end
-
-# Test exact results of sintau/costau with real and complex integer inputs (passed as floating point types)
-for T = (Float32, Float64, BigFloat, Complex), x = -3.0:3.0
-    @test @inferred(sintau(T(x))) == sign(x)*zero(T)
-    @test @inferred(costau(T(x))) == one(T)
-end
-
-# Test corner cases of sintau/costau with abnormal real inputs
-for x in (Inf, -Inf)
-    @test_throws DomainError sintau(x)
-    @test_throws DomainError costau(x)
-end
-@test isequal(@inferred(sintau(NaN)), sinpi(NaN))
-@test isequal(@inferred(costau(NaN)), cospi(NaN))
-
-# Test corner cases of sintau/costau with abnormal complex inputs
-for x in (Inf, NaN, 0), y in (Inf, NaN, 0)
-    z = complex(x, y)
-    @test isequal(@inferred(sintau(z)), sinpi(2 * z))
-    @test isequal(@inferred(costau(z)), cospi(2 * z))
-end
-
-# Check type stability of sintau/costau (adapted from julia/test/math.jl)
-for T = (Int,Float32,Float64,BigFloat), f = (sintau,costau)
-    @test Base.return_types(f,Tuple{T}) == [T]
-    @test Base.return_types(f,Tuple{Complex{T}}) == [Complex{float(T)}]
-end
-
-# Test modtau (adapted from julia/test/mod2pi.jl)
-@test_throws ArgumentError modtau(Int64(2)^60-1)
-@test modtau(10) ≈ mod(10, tau)
-@test modtau(-10) ≈ mod(-10, tau)
-@test modtau(355) ≈ 3.1416227979431572
-@test modtau(Int32(355)) ≈ 3.1416227979431572
-@test modtau(355.0) ≈ 3.1416227979431572
-@test modtau(355.0f0) ≈ 3.1416228f0
-@test modtau(Int64(2)^60) == modtau(2.0^60)
