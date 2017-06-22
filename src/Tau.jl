88fad158704c15724f2ff168d264f5a6d3026fe0
Waldir Pimenta <waldyrious@gmail.com>
2017-06-22 03:01:46 +0100

    implement equality with 2pi (in type-stable manner)
    This is done using the old method signature syntax,
    to remain compatible with 0.5

--- src/Tau.jl
+++ src/Tau.jl
@@ -11,6 +11,23 @@ export tau, τ,
 Base.@irrational τ 6.28318530717958647692 (2 * big(pi))
 const tau = τ
 
+# Implement tau == 2pi (and pi = tau/2) by defining additional signatures for the == method.
+# This overrides, for these cases only, the (always false) result
+# of `==(::Irrational, ::Real)` defined in base/irrationals.jl
+import Base.==
+=={T<:Real}(::Irrational{:τ}, n::T) = n == 2 * T(π)
+=={T<:Real}(n::T, ::Irrational{:τ}) = n == 2 * T(π)
+=={T<:Real}(::Irrational{:π}, n::T) = n == T(τ) / 2
+=={T<:Real}(n::T, ::Irrational{:π}) = n == T(τ) / 2
+# Note: we're using the method signature syntax `foo{T<:Real}(x::T) = ...` that's valid in julia 0.5,
+# instead of the new `foo(x::T) where T<:Real = ...` syntax introduced in julia 0.6,
+# to avoid breaking backward compatibility or introducing a dependency on Compat.jl
+
+# Disambiguate the `==(:`Irrational{s}, ::Irrational{s}) where {s}`
+# defined in base/irrationals.jl
+==(::Irrational{:τ}, ::Irrational{:τ}) = true
+==(::Irrational{:π}, ::Irrational{:π}) = true
+
 include("trig.jl")
 
 modtau(x) = Base.mod2pi(x)
