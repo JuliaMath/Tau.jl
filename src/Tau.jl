db2b0982ca5b8aa9e20be0dbc3148ea1494d1e94
David Widmann <devmotion@users.noreply.github.com>
2023-02-22 23:38:18 +0100

    Add `sincostau`

--- src/Tau.jl
+++ src/Tau.jl
@@ -3,8 +3,8 @@ module Tau
 import IrrationalConstants
 
 export tau, τ,
-       sintau, costau, modtau,
-       sinτ, cosτ, modτ
+       sintau, costau, sincostau, modtau,
+       sinτ, cosτ, sincosτ, modτ
 
 # Definition of τ as irrational constant
 const τ = IrrationalConstants.twoπ
@@ -16,12 +16,26 @@ const modtau = modτ
 # Trigonometric functions
 sinτ(x) = sinpi(2 * x)
 cosτ(x) = cospi(2 * x)
+function sincosτ(x)
+    y = 2 * x
+    # sincospi was added in https://github.com/JuliaLang/julia/pull/35816
+    @static if VERSION < v"1.6.0-DEV.292"
+        return (sinpi(y), cospi(y))
+    else
+        return sincospi(y)
+    end
+end
 
 # Optimization for integers
 sinτ(x::Integer) = zero(float(x))
 cosτ(x::Integer) = one(float(x))
+function sincosτ(x::Integer)
+    y = float(x)
+    return (zero(y), one(y))
+end
 
 const sintau = sinτ
 const costau = cosτ
+const sincostau = sincosτ
 
 end
